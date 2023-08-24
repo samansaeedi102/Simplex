2f9db0edc9e69379299dba0ab307263e0fc03907
samansaeedi102 <samansaeedi102@gmail.com>
2023-08-24 11:00:56 +0200

    start project and add some file structures

--- apps/simplex/src/main.ts
+++ apps/simplex/src/main.ts
@@ -1,14 +1,21 @@
 import express from 'express';
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 8000;
 
 const app = express();
+app.use(bodyParser.json());
 
+mongoose.connect("mongodb://127.0.0.1:27017/simplex")
+  .then(() => {
+    app.listen(port, host, () => {
+      console.log(`[ ready ] http://${host}:${port}`);
+    });
+  })
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  res.send(JSON.stringify({ name: "saman", family: "saeedi" }));
 });
 
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
-});
+
